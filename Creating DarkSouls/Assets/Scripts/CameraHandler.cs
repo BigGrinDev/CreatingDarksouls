0d000f394c380d60c532cf04a6c4d3c728471956
BigGrinDev <tieger5d9@gmail.com>
2024-01-17 13:40:28 +0100

    h

---
+++ Creating DarkSouls/Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    public Transform targetTransform;
+    public Transform cameraTransform;
+    public Transform cameraPivotTransform;
+    private Transform myTransform;
+    private Vector3 cameraTransformPosition;
+    private LayerMask ignoreLayers;
+
+    public static CameraHandler singleton;
+
+    public float lookSpeed = 0.1f;
+    public float followSpeed = 0.1f;
+    public float pivotSpeed = 0.03f;
+
+    private float defaultPosition;
+    private float lookingAngle;
+    private float pivotAngle;
+    private float minimumPivot = -35;
+    private float maximumPivot = 35;
+
+    private void Awake()
+    {
+        singleton = this;
+        myTransform = transform;
+        defaultPosition = cameraTransform.localPosition.z;
+        ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
+
+    }
+}
